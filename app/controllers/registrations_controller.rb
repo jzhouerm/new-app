e1fd49b38f3bfa21a61c8975adb6e99633757275
yulia <juliana.ny2008@gmail.com>
2020-07-28 09:26:26 -0400

    7/28 9.26 am, adding button Register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,12 @@ class RegistrationsController < ApplicationController
     end
 
     def create 
-@registration=Registration.create(params[:id])
+@registration=Registration.create(registration_params)
+redirect_to event_path(@registration.event_id)
+    end
+
+    private 
+    def registration_params
+    params.require(:registration).permit(:user_id, :event_id) 
     end
 end
