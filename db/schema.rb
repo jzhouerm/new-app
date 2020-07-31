32ca2d0bb30b46bab1ae130eee86405247776d23
yulia <juliana.ny2008@gmail.com>
2020-07-30 21:16:56 -0400

    7/30 6:16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_152712) do
+ActiveRecord::Schema.define(version: 2020_07_31_003248) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
